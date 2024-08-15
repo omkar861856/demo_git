7b36a861c34ccdcbc5c4c55c71979e5433dc81a8
Bhoomikak18 <133781377+Bhoomikak18@users.noreply.github.com>
2024-08-15 20:23:44 +0530

    js logic implemented

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+const input = document.getElementById("demo");
+input.addEventListener("input",(event)=>{
+   const result =document.getElementById("result");
+   result.innerText= event.target.value;
+});
